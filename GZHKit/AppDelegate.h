a6ca20ddab4867fb487f2a443fa4d7514be94e32
guzhenhuaGitHub <514668826@qq.com>
2017-05-02 11:19:18 +0800

    0.0.1
    first initialize

---
+++ GZHKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GZHKit
+//
+//  Created by 顾振华 on 2017/5/2.
+//  Copyright © 2017年 顾振华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GZHKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZHKit
+//
+//  Created by 顾振华 on 2017/5/2.
+//  Copyright © 2017年 顾振华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
